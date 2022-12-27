d730971d55288f04d5392840aaf79ba5f56d0b45
Beato <121357043+urc4@users.noreply.github.com>
2022-12-27 18:00:38 -0300

    Fix logic statements and polish the game
    Added some functionalities as to not be able to play the same
    option as played before. Introduced some outputs such that the
    players are able to tell at which stage of the game it is:
    inform when victory has been achieve, inform players to choose
    between aavailable options.

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ def start_game():
    board = {}
    for position in range(0,9):
       board[position] = ''
+
    return board
+
 
 def display_game(board):
    '''
@@ -38,11 +40,11 @@ def display_game(board):
          print('------------')
          div += 1
 
-def check_board(board, choice):
-   return board[0] == board[1] == board [2] == choice or board[3] == board[4] == board [5] == choice or \
-      board[6] == board[7] == board [8] == choice or board[0] == board[3] == board [6] == choice or \
-         board[1] == board[4] == board [7] == choice or board[2] == board[5] == board [8] == choice or \
-            board[0] == board[4] == board [8] == choice or board[2] == board[4] == board [6] == choice 
+def check_board(board, symbol):
+   return board[0] == board[1] == board [2] == symbol or board[3] == board[4] == board [5] == symbol or \
+      board[6] == board[7] == board [8] == symbol or board[0] == board[3] == board [6] == symbol or \
+         board[1] == board[4] == board [7] == symbol or board[2] == board[5] == board [8] == symbol or \
+            board[0] == board[4] == board [8] == symbol or board[2] == board[4] == board [6] == symbol 
             
 def user_choice(player):
    # Could add a vector of possible values as game progresses
@@ -50,48 +52,66 @@ def user_choice(player):
    within_range = False
    while position.isdigit() == False or within_range == False:
       position = input(f'{player} choose a number between 0 and 8: ')
+      
       if position.isdigit() == False: 
-         clear()
          print('Sorry, but you did not enter a valid positon for the board.\nPlease try again.')
+      
       else:
          if int(position) in range(0,9): within_range = True
-         else: within_range = False
+         else: 
+            within_range = False
+            print('Sorry, but you did not enter a valid positon for the board.\nPlease try again.')
+   
    return int(position)
 
 def symbol_choice():
    symbol = 'wrong'
    players = {}
    while symbol not in ['x','o']:
-      symbol = input('Playuh 1, pick x or o: ')
+      symbol = input('Player 1, pick x or o: ')
       if symbol not in ['x','o']:
-         clear()
-         print("Sorry, I didn't understand. Please make sure to choose x or o.")
+         print("Sorry, but you did not enter a valid symbol to play with.\nPlease make sure to choose x or o.")
    if symbol == 'x':
       players['player1'] = 'x'
       players['player2'] = 'o'
    else:
       players['player1'] = 'o'
       players['player2'] = 'x'
+  
    return players
+
 
 def gameon():
    board = start_game()
    players = symbol_choice()
    display_game(board)
    players_keys = list(players.keys())
-   print(players_keys)
    gameover = False
+   available_options = list(range(0,9))
    while gameover == False:
-      position1 = user_choice(players_keys[0])
+      position1 = 'wrong'
+      position2 = 'wrong'
+      while position1 not in available_options:
+         position1 = user_choice(players_keys[0])
+         print('Choose an available option')
+      available_options.pop(available_options.index(position1))
       board[position1] = players['player1']
       display_game(board)
-      gameover = check_board(board, players['player1'])
-
-      position2 = user_choice(players_keys[1])
+      if check_board(board, players['player1']):
+         gameover = True
+         print(f'{players_keys[0]} is the tic tac toe master!')
+         continue
+      while position2 not in available_options:
+         position2 = user_choice(players_keys[1])
+         print('Choose an available option')
+      available_options.pop(available_options.index(position2))
       board[position2] = players['player2']
       clear()
       display_game(board)
-      gameover = check_board(board, players['player2'])
+      if check_board(board, players['player2']):
+         gameover = True
+         print(f'{players_keys[1]} is the tic tac toe master!')
+         continue
 
       
    
@@ -101,18 +121,16 @@ def gameon_choice():
       choice = input('Would you like to keep playing? y or n.\n')
       if choice not in ['y', 'n']:
          clear()
-         print("Sorry, I didn't understand. Please make sure to choose y(es) or n(o).")
+         print("Sorry, but you did not enter a valid answer.\nPlease make sure to choose y(es) or n(o).")
       if choice == 'y': return True
       else: return False
 
 
 
-
-
+# Game logic altogether
 game_on = True
-
 while game_on:
    clear()
    gameon()
    game_on = gameon_choice()
-
+print('Thanks for playing.')
